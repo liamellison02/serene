e31b5e712a5d947963d90ac70666c8fe77ad65d0
armandrismir <adrismir@outlook.com>
2024-05-17 16:12:26 -0700

    Added gradient ball

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,9 +1,24 @@
+function GradientBall() {
+
+    const gradient = {
+        background: 'linear-gradient(45deg, #6DC543 0%, #F37C7C 150%)'
+    };
+
+    return (
+        <div id="Emotions" className="flex justify-center items-center">
+            <div id="circle" className="w-[50px] h-[50px] rounded-full" style={gradient}>
+            </div>
+            <p className="ml-4 hidden md:block text-[20px]">Looking Good 😎</p>
+        </div>
+    );
+}
+
 function Header() {
-    return(
-        <header className="w-full h-[60px] px-8 flex justify-between items-center text-[40px] md:text-64px font-serif">
+    return (
+        <header className="w-full h-[60px] px-8 flex justify-between items-center text-[40px] md:text-64px font-serif relative">
             <h2 className="">Serene</h2>
-            <div>...</div>
-            <div>•</div>
+            <GradientBall />
+            <div className="w-[50px] h-[50px] rounded-full bg-black"></div>
         </header>
     );
 }
