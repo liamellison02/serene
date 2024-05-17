76e34fa0c0831ad8a4dac55ea6259f71e1df46a3
armandrismir <adrismir@outlook.com>
2024-05-17 14:19:35 -0700

    Setup routing

---
+++ client/src/components/Feed.js
@@ -0,0 +1,7 @@
+function Feed() {
+    return (
+        <></>
+    )
+}
+
+export default Feed;
---
+++ client/src/pages/Home.js
@@ -0,0 +1,13 @@
+import Feed from "../components"
+
+function Home() {
+    return (
+        <div className="min-h-screen flex items-center justify-center">
+            <h1 className="text-4xl font-bold text-blue-500">
+                Hello, Tailwind CSS!
+            </h1>
+        </div>
+    );
+}
+
+export default Home;
