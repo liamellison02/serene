280d77f3a67bf4731c02d3b397810fa33b73aead
liamellison02 <liam.ellison.codes@gmail.com>
2024-05-18 11:05:46 -0400

    update to routing

--- api/src/user/models.py
+++ api/src/user/models.py
@@ -3,6 +3,8 @@ from passlib.hash import pbkdf2_sha256
 from src import db
 import uuid
 
+X_HOST = 'https://api.twitter.com'
+
 
 class User:
     def __init__(self):
@@ -52,3 +54,17 @@ class User:
             return self.start_session(user)
 
         return jsonify({"error": "Invalid login credentials"}), 401
+
+    def getTweetData(self):
+        if self.is_authenticated:
+            user = db.users.find_one({"username": request.json['username']})
+            twitter_id = user['user_id']
+            # make request to Twitter api to grab tweets
+            # create a dict of dicts to store tweets ex:
+            # tweet_data = {
+            #     "tweets": tweets_from_twitter_api,
+            # }
+            # perform sent analysis on tweets
+            # append analysis data to the tweet_data dict under the key "analysis"
+            # store tweet dict into database
+            # return jsonify(tweet_data), 200
--- api/src/user/routes.py
+++ api/src/user/routes.py
@@ -1,17 +1,24 @@
 from src import app
 from src.user.models import User
+from flask import request
 
 
-@app.route('/user/signup', methods=['POST'])
+@app.route('/signup', methods=['POST'])
 def signup():
     return User().signup()
 
 
-@app.route('/user/logout')
+@app.route('/logout')
 def logout():
     return User().logout()
 
 
-@app.route('/user/login', methods=['POST'])
+@app.route('/login', methods=['POST'])
 def login():
     return User().login()
+
+
+@app.route('/authorize/twitter', methods=['GET'])
+def twitter_info():
+    args = request.args
+    return args
