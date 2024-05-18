8579b5bbb2e72895816683ed7631b44570c98dc7
armandrismir <adrismir@outlook.com>
2024-05-17 23:25:49 -0700

    Feed now matches backend specs

--- client/src/components/Feed.js
+++ client/src/components/Feed.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
 
-function Tweet({name, content}) {
+function Tweet({user, text}) {
     return (
         <div id="tweet" className="my-10">
-            <p className="text-[#00297A] font-bold text-xl"><b>{name}</b></p>
-            <p className="text-xl">{content}</p>
+            <p className="text-[#00297A] font-bold text-xl"><b>{user}</b></p>
+            <p className="text-xl">{text}</p>
         </div>
     );
 }
@@ -13,16 +13,28 @@ function Feed() {
 
     const [tweets, setTweets] = useState([
         {
-            name: "@SummoningSalt",
-            content: "1 million views in 40 hours! Not bad for a 2 hour long video. Thanks so much everyone - super positive feedback on this one. More Tetris content in the future for sure.",
+            "mood": "sad",
+            "score": "69420",
+            "id":"343434",
+            "date":"2020-02-28",
+            "user": "@SummoningSalt",
+            "text": "1 million views in 40 hours! Not bad for a 2 hour long video. Thanks so much everyone - super positive feedback on this one. More Tetris content in the future for sure.",
         },
         {
-            name: "@YikeLOL",
-            content: "That really hurts",
+            "mood": "sad",
+            "score": "69420",
+            "id":"343434",
+            "date":"2020-02-28",
+            "user": "@YikeLOL",
+            "text": "That really hurts",
         },
         {
-            name: "User3",
-            content: "Split 2 ADC SoloQ TierlistChamps with high AD scalings that don't need Attackspeed do really well right now - Cait Jhin Aphelios Draven BLACKFIRE TORCH is absolutely broken hence Karthus and Brand",
+            "mood": "sad",
+            "score": "69420",
+            "id":"343434",
+            "date":"2020-02-28",
+            "user": "@User3",
+            "text": "Split 2 ADC SoloQ TierlistChamps with high AD scalings that don't need Attackspeed do really well right now - Cait Jhin Aphelios Draven BLACKFIRE TORCH is absolutely broken hence Karthus and Brand",
         }
     ]);
 
@@ -30,7 +42,7 @@ function Feed() {
         <div id="feed" className="w-[85%] md:w-[600px] h-full">
             {tweets.map((tweet, index) => (
                 <div key={index}>
-                    <Tweet name={tweet.name} content={tweet.content}/>
+                    <Tweet user={tweet.user} text={tweet.text}/>
                 </div>
             ))}
         </div>
