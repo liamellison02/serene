282f49a835a978de28dacdffafb40a180f22fb3f
liamellison02 <liam.ellison.codes@gmail.com>
2024-05-18 12:30:34 -0400

    pseudocode for retrieving analysis data

--- api/src/user/models.py
+++ api/src/user/models.py
@@ -1,9 +1,20 @@
-from flask import jsonify, request, session, redirect
+from flask import jsonify, request, session, redirect, url_for
 from passlib.hash import pbkdf2_sha256
 from src import db
 import uuid
+import oauth2 as oauth
+import urllib.parse
+import urllib.request
+import urllib.error
+import json
 
-X_HOST = 'https://api.twitter.com'
+request_token_url = 'https://api.twitter.com/oauth/request_token'
+access_token_url = 'https://api.twitter.com/oauth/access_token'
+authorize_url = 'https://api.twitter.com/oauth/authorize'
+show_user_url = 'https://api.twitter.com/1.1/users/show.json'
+authorize_endpoint = 'tweet.read%20users.read%20follows.read%20offline.access'
+
+oauth_creds = {}
 
 
 class User:
@@ -44,7 +55,7 @@ class User:
         self.is_authenticated = False
         return redirect('/')
 
-    def login(self):
+    def login_with_twitter(self):
 
         user = db.users.find_one({
             "username": request.json['username']
@@ -59,12 +70,42 @@ class User:
         if self.is_authenticated:
             user = db.users.find_one({"username": request.json['username']})
             twitter_id = user['user_id']
-            # make request to Twitter api to grab tweets
-            # create a dict of dicts to store tweets ex:
-            # tweet_data = {
-            #     "tweets": tweets_from_twitter_api,
-            # }
+
+            # user_tweets_from_api = [
+            #     {
+            #         "id": 343434,
+            #         "date": "2020-02-28",
+            #         "user": "liamellisonrocks",
+            #         "text": "man i love to code"
+            #      }
+            # ]
+            # timeline_tweets_from_api = [
+            #     {
+            #         "id": 323423334,
+            #         "date": "2023-02-01",
+            #         "user": "elonmusk",
+            #         "text": "i own twitter"
+            #      }
+            # ]
             # perform sent analysis on tweets
-            # append analysis data to the tweet_data dict under the key "analysis"
+            # overall_results = {
+            #     "total_anger_intensity_level": "number here",
+            #     "total_joy_intensity_level": "number here",
+            #     "total_sadness_intensity_level": "number here",
+            #     # etc...
+            #     "timeline_anger_intensity_level": "number here",
+            #     "timeline_joy_intensity_level": "number here",
+            #     "timeline_sadness_intensity_level": "number here"
+            #     # etc...
+            # }
+            # predicted_sentiment_per_tweet = [
+            #     {
+            #         "tweet_id": 343434,
+            #         "predicted_sentiment": "emotion",
+            #         "intensity_level": "probability x polarity score"
+            #     }
+            # ]
+            # user_analysis_data = (overall_results, predicted_sentiment_per_tweet)
+
             # store tweet dict into database
             # return jsonify(tweet_data), 200
