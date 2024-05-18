c24ee93c9948ff1ced05238d7106d599610124d5
ericl574 <bea2792935061@gmail.com>
2024-05-18 16:48:31 -0700

    Update Navbar.js

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -24,7 +24,7 @@ const Navbar = () => {
   }
 
   return (
-    <header className="h-[10vh] bg-[#f8f7f1ea] backdrop-blur-md w-full sticky top-0 left-0 right-0 z-[100]">
+    <header className="h-[10vh] bg-[#FFFFFFea] backdrop-blur-md w-full sticky top-0 left-0 right-0 z-[100]">
       <nav className="h-full">
         <div className="h-full flex items-center justify-between py-4 md:px-8 px-4">
           <div className="h-full font-bold cursor-pointer ">
@@ -51,15 +51,15 @@ const Navbar = () => {
 
           <button
             onClick={toggleMobileMenu}
-            className="lg:hidden text-[#706cac] text-3xl "
-          >
+            className="lg:hidden text-[#706cac] text-3xl ">
+          
             <HiOutlineMenu />
           </button>
         </div>
 
         {isMenuOpen && (
           <div className="items-end">
-           <div className="absolute right-0 w-[110px] bg-[#F8F7F1] text-[#00297A] text-[20px] font-bold rounded py-4">
+           <div className=" absolute right-0 w-[110px] bg-[#F8F7F1] text-[#00297A] text-[20px] font-bold rounded py-4">
             
             <a href="/" className="block hover:text-grey-400 my-2 mx-4">
               Home
