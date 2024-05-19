a3f3ca223962b19158188d248b0585fb95849f79
armandrismir <adrismir@outlook.com>
2024-05-19 02:14:40 -0700

    Add to tweet data and make feed look nicer

--- client/src/components/Charts/Chart1.js
+++ client/src/components/Charts/Chart1.js
@@ -5,7 +5,7 @@ function Chart1() {
   const timeline_data = GetTweetData()["timeline_analysis_data"];
 
   return (
-    <div className="w-full h-full rounded-lg bg-red-200 p-6 overflow-y-scroll">
+    <div className="w-full h-full rounded-lg bg-amber-100 p-6 overflow-y-scroll">
       <h1 className="text-xl mb-4">Quick Timeline Info</h1>
       <p># Tweets: <b>{timeline_data["num_timeline_tweets"]}</b></p>
       <p>Overall Emotion: <b>{timeline_data["timeline_most_prominent_emotion"]}</b></p>
