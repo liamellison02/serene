aa368ab056dd0d976975467cfc286bdff932ded6
armandrismir <adrismir@outlook.com>
2024-05-19 06:51:26 -0700

    Two timelines

--- client/src/components/Charts/BarGraph.js
+++ client/src/components/Charts/BarGraph.js
@@ -61,7 +61,7 @@ function BarGraph() {
     ],
   };
   return (
-    <div className="w-full h-full bg-slate-200 rounded-lg flex justify-center items-center">
+    <div className="w-full h-full bg-slate-200 rounded-lg flex justify-center items-center p-4">
         <Bar data={chartData} options={options}/>
     </div>
   );
--- client/src/components/Feed.js
+++ client/src/components/Feed.js
@@ -9,15 +9,22 @@ function Tweet({user, text, date}) {
     );
 }
 
-function Feed() {
+function Feed({type}) {
 
-    const user_tweets = GetTweetData()["user_tweet_data"];
-    const timeline_tweets = GetTweetData()["timeline_tweet_data"];
+    console.log(type)
 
-    const tweets = [...user_tweets, ...timeline_tweets];
+    let tweets = [];
+
+    if (type === "user" && GetTweetData()["has_user_data"]) {
+        tweets = GetTweetData()["user_tweet_data"];
+        console.log(GetTweetData()["user_tweet_data"])
+    }
+    else {
+        tweets = GetTweetData()["timeline_tweet_data"];
+    }
 
     return (
-        <div id="feed" className="w-[85%] md:w-[600px] h-full overflow-y-scroll overflow-x-hidden">
+        <div id="feed" className="w-[85%] md:w-[33%] h-full overflow-y-scroll overflow-x-hidden">
             {tweets.map((tweet, index) => (
                 <div key={index}>
                     <Tweet user={tweet.user} text={tweet.text} date={tweet.date}/>
