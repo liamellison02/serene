a58027643cfb6460cace75fd6f5cc6368bbef93a
Liam Ellison <liam.ellison.codes@gmail.com>
2024-06-01 15:18:55 -0400

    updated analyze route

--- api/flaskapp/api.py
+++ api/flaskapp/api.py
@@ -7,15 +7,29 @@ api_bp = Blueprint('api', __name__)
 def analyze():
     user_id = request.args.get('user_id')
     data = db.users.find_one({"user_id": user_id})
-    # model = current_app.config['sentiment_model']
-    # sequence = model.get_sequences([text])[0]
-    # sentiment = model.predict_emotion(sequence)
-    # intensity = model.predict_emotion_probability(sequence)
     user_tweets = data['user_tweets']
     user_timeline = data['user_timeline']
-    sentiment_data = {'overall_sentiment': 'joy'}
+    model = current_app.config['sentiment_model']
+    
+    user_text = [tweet['text'] for tweet in user_tweets]
+    user_sequences = model.get_sequences(user_text)
+    for i in range(len(user_tweets)):
+        sequence = user_sequences[i]
+        sentiment = model.predict_emotion(sequence)
+        intensity = model.predict_emotion_probability(sequence) * model.get_sentiment_score(user_text[i])
+        user_tweets[i]['sentiment'] = sentiment
+        user_tweets[i]['intensity'] = intensity
+    
+    timeline_text = [tweet['text'] for tweet in user_timeline]
+    timeline_sequences = model.get_sequences(timeline_text)
+    for i in range(len(user_timeline)):
+        sequence = timeline_sequences[i]
+        sentiment = model.predict_emotion(sequence)
+        intensity = model.predict_emotion_probability(sequence) * model.get_sentiment_score(timeline_text[i])
+        user_timeline[i]['sentiment'] = sentiment
+        user_timeline[i]['intensity'] = intensity
+    
     return jsonify({
         "user_tweets": user_tweets,
-        "timeline_tweets": user_timeline,
-        "sentiment_data": sentiment_data
+        "user_timeline": user_timeline
     })
