f7fb15e6ad5ca042f88c57d0436586dc54330f3e
armandrismir <adrismir@outlook.com>
2024-06-03 00:58:49 -0700

    new packages & styles

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -8,6 +8,7 @@ module.exports = {
       fontFamily: {
         'playfair': ["Playfair Display", 'serif'],
         'lora': ["Lora", 'serif'],
+        'mulish': ["Mulish", 'sans-serif']
       }
     },
   },
