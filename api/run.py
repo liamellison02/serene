a150ae321ec912c3b37a024a1eb5a00d9b1e1725
armandrismir <adrismir@outlook.com>
2024-06-06 22:51:48 -0700

    Dont train model

--- api/run.py
+++ api/run.py
@@ -1,5 +1,9 @@
+import dotenv
+dotenv.load_dotenv(override=True)
+
 import os
 from flaskapp import create_app
+from flaskapp.process import TweetSentimentAnalyzer
 
 app = create_app()
 app.secret_key = os.urandom(50)
@@ -12,4 +16,10 @@ if __name__ == '__main__':
     else:
         port = int(port)
         print(f"Using port {port} from PORT environment variable.")
+
     app.run(host="0.0.0.0", port=port)
+    # Initialize and train the sentiment model
+    train_path = os.environ.get('TRAIN_PATH')
+    val_path = os.environ.get('VAL_PATH')
+    test_path = os.environ.get('TEST_PATH')
+    app.config['sentiment_model'] = TweetSentimentAnalyzer(train_path, val_path, test_path)
