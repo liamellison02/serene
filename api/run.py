2b15b5e1f674b7f88da67daea28c00caadb4c7f0
armandrismir <adrismir@outlook.com>
2024-06-06 22:33:23 -0700

    Port env variable debugging

--- api/run.py
+++ api/run.py
@@ -9,11 +9,21 @@
 #     app.run(debug=True, host='0.0.0.0', port=port)
 
 from flask import Flask
+import os
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return 'Hello, World!'
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=3000)
+if __name__ == "__main__":
+    port = os.environ.get("PORT")
+
+    if port is None:
+        port = 5000
+        print("PORT environment variable not set. Falling back to default port 5000.")
+    else:
+        port = int(port)
+        print(f"Using port {port} from PORT environment variable.")
+    
+    app.run(host="0.0.0.0", port=port)
