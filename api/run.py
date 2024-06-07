e6a1e15c6a6ea0db1fe93cdeb4a32819997eaf7b
armandrismir <adrismir@outlook.com>
2024-06-06 22:16:19 -0700

    Minimal flask app

--- api/run.py
+++ api/run.py
@@ -1,9 +1,19 @@
-import os
-from flaskapp import create_app
+# import os
+# from flaskapp import create_app
 
-app = create_app()
-app.secret_key = os.urandom(50)
+# app = create_app()
+# app.secret_key = os.urandom(50)
+
+# if __name__ == '__main__':
+#     port = int(os.environ.get("PORT", 5000))
+#     app.run(debug=True, host='0.0.0.0', port=port)
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello, World!'
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(debug=True, host='0.0.0.0', port=5000)
