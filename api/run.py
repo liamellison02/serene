696a4fb4747f028b1c5b140d416010770db07cba
armandrismir <adrismir@outlook.com>
2024-06-06 22:41:28 -0700

    Full app with port debug info

--- api/run.py
+++ api/run.py
@@ -1,29 +1,15 @@
-# import os
-# from flaskapp import create_app
-
-# app = create_app()
-# app.secret_key = os.urandom(50)
-
-# if __name__ == '__main__':
-#     port = int(os.environ.get("PORT", 5000))
-#     app.run(debug=True, host='0.0.0.0', port=port)
-
-from flask import Flask
 import os
-app = Flask(__name__)
+from flaskapp import create_app
 
-@app.route('/')
-def hello():
-    return 'Hello, World!'
+app = create_app()
+app.secret_key = os.urandom(50)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     port = os.environ.get("PORT")
-
     if port is None:
         port = 5000
         print("PORT environment variable not set. Falling back to default port 5000.")
     else:
         port = int(port)
         print(f"Using port {port} from PORT environment variable.")
-    
     app.run(host="0.0.0.0", port=port)
