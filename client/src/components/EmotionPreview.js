92d05d9205803dbaf5b1b21cf158b8196a3abade
armandrismir <adrismir@outlook.com>
2024-06-18 20:56:09 -0700

    Make EmotionPreview tailored for backend data with error checking

--- client/src/components/EmotionPreview.js
+++ client/src/components/EmotionPreview.js
@@ -1,34 +1,40 @@
 import { mix } from 'polished'
 import { useSelector } from 'react-redux'
+import { useState, useEffect } from 'react'
 
 function EmotionPreview() {
 
     const tweetData = useSelector(state => state.tweetData)
-    const data = tweetData.data
+    let data = tweetData.data
 
     // Love color: #fe43ef
     // Anger color: #FF4500
     // Sadness color: #4682B4
 
-    const love_info = {
-        "adjective": "happy",
-        "noun":"happiness",
+    const positive_info = {
+        "adjective": "positive",
+        "noun":"positivity",
         "hex": "#FE43EF",
         "value": undefined,
     }
 
-    const anger_info = {
-        "adjective": "angry",
-        "noun": "anger",
-        "hex": "#FF4500",
+    const neutral_info = {
+        "adjective": "neutral",
+        "noun": "neutrality",
+        "hex": "#a0ebab",
         "value": undefined,
     }
 
-    const sadness_info = {
-        "adjective": "sad",
-        "noun": "sadness",
+    const negative_info = {
+        "adjective": "negtaive",
+        "noun": "negativity",
         "hex": "#4682B4",
         "value": undefined
+    }
+
+    const na_info = {
+        "adjective": "miscellaneous",
+        "noun": "miscellaneous tweets"
     }
 
     let primary = {
@@ -43,18 +49,16 @@ function EmotionPreview() {
         "hex": "#5E5E5E",
         "value": undefined,
     }
-        
-    if (data) {
-        let love = 0, anger = 0, sadness = 0;
-        ({ love, anger, sadness } = data["timeline_sentiment_data"]["intensity_totals"]);
-        if (!love) { love = 0 }
-        if (!anger) { anger = 0 }
-        if (!sadness) {sadness = 0}
-        love_info.value = love
-        anger_info.value = anger
-        sadness_info.value = sadness
+    
+    let Positive = 0, Neutral = 0, Negative = 0, NA = 0;
+    ({ Positive, Neutral, Negative, NA } = data["timeline_sentiment_data"]["intensity_totals"]);
+    if (Positive || Neutral || Negative || NA) {
+        positive_info.value = Positive
+        neutral_info.value = Neutral
+        negative_info.value = Negative
+        na_info.value = NA
 
-        const emotions = [love_info, anger_info, sadness_info];
+        const emotions = [positive_info, neutral_info, negative_info, na_info];
         const sortedEmotions = emotions.sort((a, b) => b.value - a.value);
         [primary, secondary] = sortedEmotions.slice(0, 2);
     }
@@ -71,17 +75,23 @@ function EmotionPreview() {
         color: mix(0.6, 'black', primary.hex),
     }
 
+    const [message, setMessage] = useState("Loading tweets...")
+
+    useEffect(() => {
+        if (data && (Positive || Neutral || Negative || NA)) {
+            setMessage(`Your timeline is mostly ${primary.adjective}, with a bit of ${secondary.noun}`)
+        } else {
+            setMessage("The backend returned data but we were not able to process it 🤒")
+        }
+    }, [data, Positive, Neutral, Negative, NA, primary.adjective, secondary.noun])
+
     return (
         <div id="Emotions" className="h-full w-full flex flex-col justify-evenly items-center px-5" style={{...bgGradient, ...textColor}}>
             <div className="w-full flex items-center justify-center ">
                 <h1 className="text-[64px] font-semibold leading-[1rem]">SERENE</h1>
             </div>
             <div id="circle" className="w-[45%] pb-[45%] rounded-full font-sans" style={gradient} />
-            {data ? (
-                <p className="text-center text-[34px] w-[85%]">Your timeline is mostly {primary.adjective}, with a bit of {secondary.noun}.</p>
-            ) : (
-                <p className="text-center text-[34px] w-[85%]">Loading tweets...</p>
-            )}
+            <p className="text-center text-[34px] w-[85%]">{message}</p>
         </div>
     );
 }
